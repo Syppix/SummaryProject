fbb1f2188fa6973a9d50a8ea7c139e9c1a1556e2
Syppix <kln.19@uni-dubna.ru>
2022-05-29 19:18:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function script(){
+$('.menu-button')
+    $('.menu').toggleClass('menu_active');
+    $('.main-box').toggleClass('main-box_active');
+}
+
+function myFunction(){
+    document.getElementById('paragraph').innerHTML = 'Hello';
+}
+
+
